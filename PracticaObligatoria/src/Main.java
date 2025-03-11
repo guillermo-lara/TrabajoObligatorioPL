ea3a63e4bc0471add3165860551a2b35b8dd9bdb
dvcoronado <d.coronado.2022@alumnos.urjc.es>
2025-03-11 11:45:23 +0100

    Añadidos ficheros

--- PracticaObligatoria/src/Main.java
+++ PracticaObligatoria/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+
+            // aplicar la gramatica a la frase
+            CharStream input = CharStreams.fromFileName(args[0]);
+            GramaticaLexer lexer = new GramaticaLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            GramaticaParser parser = new GramaticaParser(tokens);
+
+            parser.axioma();
+
+        } catch (org.antlr.v4.runtime.RecognitionException e) {
+//Fallo al reconocer la entrada
+            System.err.println("REC " + e.getMessage());
+        } //catch (IOException e) {
+//Fallo de entrada/salida
+        //System.err.println("IO " + e.getMessage());
+        //}
+        catch (java.lang.RuntimeException e) {
+//Cualquier otro fallo
+            System.err.println("RUN " + e.getMessage());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     }
 }
